4962851483a8b09e160581b3325c5b508fb0f9fc
YanLeon <juan_12086@hotmail.com>
2026-03-22 20:27:27 -0500

    Cambio realizado

--- packages/Webkul/BoldPayment/src/Http/routes.php
+++ packages/Webkul/BoldPayment/src/Http/routes.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Webkul\BoldPayment\Http\Controllers\PaymentController;
 
-Route::group(['middleware' => ['web', 'theme', 'locale', 'currency', 'channel']], function () {
+Route::group(['middleware' => ['web', 'locale', 'theme', 'currency']], function () {
     Route::get('/bold/checkout', [PaymentController::class, 'checkout'])->name('bold.checkout');
     Route::get('/bold/callback', [PaymentController::class, 'callback'])->name('bold.callback');
     Route::post('/bold/generate-signature', [PaymentController::class, 'generateSignature'])->name('bold.generate.signature');
